6e0d1c75c7aa64dc5d37575b04079dec6e89d8e9
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-11-08 17:24:52 -0600

    Corrección de errores y Clase producto

---
+++ Producto.java
@@ -0,0 +1,38 @@
+public abstract class Producto {
+    int precio;
+    int serie;
+    String marca;
+    String fabDate;
+    int marcaAr;
+
+    public Producto(int precio, int serie, String marca, String fabDate, int marcaAr){
+        this.precio = precio;
+        this.serie = serie;
+        this.marca = marca;
+        this.fabDate = fabDate;
+        this.marcaAr = marcaAr;
+    }
+
+    public int getPrecio(){
+        return precio;
+    }
+
+    public int getSerie(){
+        return serie;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getFabDate(){
+        return fabDate;
+    }
+
+    public int getMarcaAr(){
+        return marcaAr;
+    }
+
+    public abstract void execute();
+
+}
