59802b1b7c9064f37e1ceb76d4b888d11793ed02
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-11-09 17:19:21 -0600

    Update Controlador.java

--- Classes/Controlador.java
+++ Classes/Controlador.java
@@ -20,25 +20,63 @@ public class Controlador {
         catalogo.add(new Cellphone(0, 0, "patito", "date", 0));
         catalogo.add(new Phone(0, 0, "patito", "date", 0));
         catalogo.add(new Camera(0, 0, "patito", "date", 0));
-        catalogo.add(new Smartphone(0, 0, "patito", "date", 0));
-        catalogo.add(new Smartphone(0, 0, "patito", "date", 0));
-
-
-        
+        catalogo.add(new Desktop(0, 0, "patito", "date", 0));
+        catalogo.add(new Laptop(0, 0, "patito", "date", 0));
+        catalogo.add(new SmartTV(0, 0, "patito", "date", 0));
+        catalogo.add(new Tablet(0, 0, "patito", "date", 0));
+        catalogo.add(new SmartWatch(0, 0, "patito", "date", 0));
         
         Vista v = new Vista();
-        //ArrayList<producto> carrito=new ArrayList<producto>(); 
+        ArrayList<Producto> carrito = new ArrayList<Producto>(); 
         //Bienvenida
         //Menu de inicio
         // ver productos
         switch (v.menuOpciones2()) {
-            case 1:
-                v.menuPrductos();// devuelve un int. 
-
-                
+            case 2:// Probando
+                catalogo.get(v.menuPrductos()-1).execute();
                 break;
                 
-            case 2:
+            case 1:// comprando jaja
+                switch (v.menuPrductos()) {
+                    case 1:// Smartphone
+                    carrito.add(new Smartphone(4000, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 2:// 
+                    carrito.add(new Cellphone(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 3:
+                    carrito.add(new Phone(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 4:
+                    carrito.add(new Camera(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 5:
+                    carrito.add(new Desktop(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 6:
+                    carrito.add(new Laptop(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 7:
+                    carrito.add(new SmartTV(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 8:
+                    carrito.add(new Tablet(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+
+                    case 9:
+                    carrito.add(new SmartWatch(900, 03503, "ISamsung", "Ayer", 0353));
+                    break;
+                    
+
+
+                }
 
                 break;
             
