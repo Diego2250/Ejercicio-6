4940aa5a3992334e51195f160e2a75eb4acaa454
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-11-09 17:19:08 -0600

    Update Controlador.java

--- Classes/Controlador.java
+++ Classes/Controlador.java
@@ -11,27 +11,72 @@ usuario.
 import java.util.ArrayList;
 
 public class Controlador {
+    
     public static void main(String[] args){
+        
+        //Iniciando catálogo. 
+        ArrayList <Producto> catalogo = new ArrayList<Producto>();
+        catalogo.add(new Smartphone(0, 0, "patito", "date", 0));
+        catalogo.add(new Cellphone(0, 0, "patito", "date", 0));
+        catalogo.add(new Phone(0, 0, "patito", "date", 0));
+        catalogo.add(new Camera(0, 0, "patito", "date", 0));
+        catalogo.add(new Smartphone(0, 0, "patito", "date", 0));
+        catalogo.add(new Smartphone(0, 0, "patito", "date", 0));
+
+
+        
+        
         Vista v = new Vista();
         //ArrayList<producto> carrito=new ArrayList<producto>(); 
-        int opcion = 0;
-        v.bienvenida();
-        while (opcion != 5){
-            opcion = v.menuOpciones();
-			switch(opcion){
-                case 1:
-                break; 
-                case 2:
+        //Bienvenida
+        //Menu de inicio
+        // ver productos
+        switch (v.menuOpciones2()) {
+            case 1:
+                v.menuPrductos();// devuelve un int. 
+
+                
                 break;
-                case 3:
+                
+            case 2:
+
                 break;
-                case 4:
-                v.despedida();
-                break;
-                default:
-				v.opcionInvalida();
-				break;
-            }
+            
         }
     }
+
+    /**
+     * menu de inicio
+     * Ver el carrito...
+     * ver productos, se despliega la lista de productos que hay. 
+     *      probar producto
+     *      añadir al carrito
+     * 
+     * 
+     * 
+     * 
+     *
+     */
+
+    
+    
+    
+    public void menuProductos(int opcion2, Producto producto){ // para añadir producto es necesario un método de la vista que compare. 
+        switch (opcion2) {
+            case 1://probar producto
+                producto.execute();
+                break;
+
+            case 2:
+                // añadir al carrito. 
+                break;
+            
+        }
+    }
+
+    
+
+
+
+
 }
