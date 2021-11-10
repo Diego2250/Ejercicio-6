3bbe26c4e97431630f75ab1b73a75c61271238fc
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-11-09 18:14:43 -0600

    a

--- Classes/Factura.java
+++ Classes/Factura.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 public class Factura {
     private String nombreCliente, fecha; 
     private int NIT, NoFactura, Total; 
-    Vista v=new Vista(); 
+    Vista v=new Vista();
+    private int total = 0;
+    ArrayList<String> articulos = new ArrayList<String>();
+    ArrayList<Integer> precios = new ArrayList<Integer>();
 
     public Factura(String nombre, String fech, int nit, int factura, int tot){
         nombreCliente=nombre;
@@ -16,14 +19,31 @@ public class Factura {
     }
 
     public void ImprimirFactura() {
-        
+        Vista v = new Vista();
+        v.print("");
     }
 
-    public void caja(ArrayList<Producto> carrito){
-        
+    public void cuenta(ArrayList<Producto> carrito){ // esta cosa se llama ANTES DE PAGaR
+        for(int i = 0; i<carrito.size(); i++){
+            sumPrecio(carrito.get(i).getPrecio());
+            articulos.add(carrito.get(i).getName());
+            precios.add(carrito.get(i).getPrecio());
+        }
     }
 
-    public 
+    public void sumPrecio(int nuevo){
+        this.total = total + nuevo;
+    }
+
+    public ArrayList<String> getArticulos(){
+        return articulos;
+    }
+
+    public ArrayList<Integer> getPrecios(){
+        return precios;
+    }
+
+    
 
 
 }
